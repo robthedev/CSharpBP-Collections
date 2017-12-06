19f4aa24836bb7e102d9db381a51893d5a7adb50
robthewebdev <robthewebdev@gmail.com>
2017-12-05 21:42:53 -0600

    udated to generic type

--- AcmeApp/Acme.Common/OperationResult.cs
+++ AcmeApp/Acme.Common/OperationResult.cs
@@ -4,19 +4,20 @@
     /// Provides a success flag and message 
     /// useful as a method return type.
     /// </summary>
-    public class OperationResult
+    public class OperationResult<T>
     {
         public OperationResult()
         {
         }
 
-        public OperationResult(bool success, string message) : this()
+        public OperationResult(T success, string message) : this()
         {
             this.Success = success;
             this.Message = message;
         }
 
-        public bool Success { get; set; }
+        //T is used to represent a generic
+        public T Success { get; set; }
         public string Message { get; set; }
     }
 
